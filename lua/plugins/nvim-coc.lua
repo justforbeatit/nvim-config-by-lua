002232093d034de1be2745f410dfa880eaf61f78
justforbeatit <justforbeatit@gmail.com>
2022-01-09 20:29:51 +0800

    updated coc config

--- lua/plugins/nvim-coc.lua
+++ lua/plugins/nvim-coc.lua
@@ -1,28 +1,15 @@
 --Use tab for trigger completion with characters ahead and navigate.
 --Make <CR> auto-select the first completion item and notify coc.nvim to
-
 Coc = {}
-
---[[
-        function! s:check_back_space() abort
-          let col = col('.') - 1
-          return !col || getline('.')[col - 1]  =~# '\s'
-        endfunction
-        inoremap <silent><expr> <TAB>
-            \ pumvisible() ? "\<C-n>" :
-            \ check_back_space() ? "\<TAB>" :
-            \ coc#refresh()
-        inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-        inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
-            \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
-
---]]--
-
 
 function Coc.setup(config)
     vim.g.coc_extensions = config.extensions
     vim.cmd([[
-        inoremap <silent><expr> <TAB> pumvisible() ? coc#_select_confirm()
+        inoremap <silent><expr> <TAB>
+            \ pumvisible() ? "\<C-n>" : "\<TAB>"
+        inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+        inoremap <silent><expr> <c-space> coc#refresh()
+        inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
             \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
     ]])
 end
